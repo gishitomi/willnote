155fa7b3d2cfe5715b33d1233e059d59be72c1e6
宜志富紹太 <shotagishitomi@gmail.com>
2021-03-09 00:11:38 +0900

    vueをインストール

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// APIのURL以外のリクエストに対してはindexテンプレートを返す
+// 画面遷移はフロントエンドのVueRouterが制御する
+// Route::get('/{any?}', function() => view('index'))->where('any', '.+');
+Route::get('/{any?}', function() {
+    return view('index');
+});
--- backend/webpack.mix.js
+++ backend/webpack.mix.js
@@ -11,7 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.browserSync({
+        proxy: '127.0.0.1:10080',
+        open: false
+    })
+    .js('resources/js/app.js', 'public/js')
+    .version()
